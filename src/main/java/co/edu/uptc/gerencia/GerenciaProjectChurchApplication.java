42fcc96863b563f981eff5b5a58aea3c8f729f0c
danielCarrilloDaza20 <daniel.carrillo01@uptc.edu.co>
2024-05-27 17:08:26 -0500

    Primer commit

---
+++ src/main/java/co/edu/uptc/gerencia/GerenciaProjectChurchApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.gerencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciaProjectChurchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciaProjectChurchApplication.class, args);
+    }
+
+}
