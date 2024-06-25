9d110ad680641cb949edf20b1f34cfcb59f3d204
danielCarrilloDaza20 <daniel.carrillo01@uptc.edu.co>
2024-06-25 10:07:18 -0500

    merge ejecutado con rama Bayron

--- src/main/java/co/edu/uptc/gerencia/model/User.java
+++
@@ -1,4 +0,0 @@
-package co.edu.uptc.gerencia.model;
-
-public class User {
-}
--- src/main/java/co/edu/uptc/gerencia/repos/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package co.edu.uptc.gerencia.repos;
-
-import co.edu.uptc.gerencia.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
