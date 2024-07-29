033e120868e118e698af89b8a1e64a74b023a1d4
danielCarrilloDaza20 <daniel.carrillo01@uptc.edu.co>
2024-07-29 10:20:52 -0500

    Adiciones importantes

--- src/main/java/co/edu/uptc/gerencia/controllers/HomeController.java
+++ src/main/java/co/edu/uptc/gerencia/controllers/HomeController.java
@@ -1,11 +1,27 @@
 package co.edu.uptc.gerencia.controllers;
 
+import co.edu.uptc.gerencia.entity.Course;
+import co.edu.uptc.gerencia.entity.Student;
+import co.edu.uptc.gerencia.repos.CourseRepository;
+import co.edu.uptc.gerencia.repos.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @Autowired
+    private StudentRepository studentRepository;
 
     @GetMapping("/landing")
     public String showLandingPage() {
@@ -13,7 +29,10 @@ public class HomeController {
     }
 
     @GetMapping({"/home", "", "/"})
-    public String home(){
+    public String home(Model model){
+        List<Course> courses = new ArrayList<>();
+        courseRepository.findAll().forEach(courses::add);
+        model.addAttribute("courses", courses);
         return "home";
     }
 
@@ -24,8 +43,12 @@ public class HomeController {
     }
 
     // request mapping para /students
-    @GetMapping({"/students"})
-    public String showStudents(){
+    @GetMapping({"/students/{courseId}"})
+    public String showStudents(@PathVariable("courseId") Long courseId, Model model){
+        Course course = courseRepository.findById(courseId).get();
+        List<Student> students = studentRepository.findByCourse(courseId);
+        model.addAttribute("course", course);
+        model.addAttribute("students", students);
         return "students";
     }
 }
--- src/main/java/co/edu/uptc/gerencia/entity/Student.java
+++ src/main/java/co/edu/uptc/gerencia/entity/Student.java
@@ -9,7 +9,7 @@ import lombok.Data;
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     @OneToOne
     @JoinColumn(name = "user_id", nullable = false)
